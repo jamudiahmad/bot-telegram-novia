374ef8c7f1ff41def2c6329914697c0217180930
jamudiahmad <jamudiahmad90@gmail.com>
2026-07-25 04:37:44 -0400

    Usando gemini-1.5-flash

--- index.js
+++ index.js
@@ -1166,7 +1166,7 @@ bot.onText(/\/ia (.+)/, async (msg, match) => {
     const prompt = match[1];
 
     try {
-        const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
         const result = await model.generateContent(prompt);
         const response = await result.response;
         const respuestaTexto = response.text();
